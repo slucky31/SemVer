b13198e98f1c86ecc73d1513e4733db83db1c1c4
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2024-01-21 15:19:09 +0100

    chore!: Init git

---
+++ SemVer/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("SemVer")]
+[assembly: AssemblyCopyright("Copyright © Nicolas DUFAUT 2024")]
+
+// x-release-please-start-version
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
+// x-release-please-end
