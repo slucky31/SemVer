bd043c589b92f341c79e33f98fdf35fac372e1dc
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2024-02-09 22:29:22 +0100

    feat: DirectoryPackage (#15)

--- SemVer/Program.cs
+++ SemVer/Program.cs
@@ -1,11 +1,17 @@
 using SemVer.Components;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
 
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
